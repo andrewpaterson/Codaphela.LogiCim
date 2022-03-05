acf7acacb40862743c0a9841bcdce7805e25362a
Andrew Paterson <andrew.ian.paterson@gmail.com>
2022-03-05 17:29:14 +0200

    Trying to read Logisim XML files.

--- LogiCim/LogisimFileReader.h
+++ LogiCim/LogisimFileReader.h
@@ -1,1 +1,18 @@
-#pragma once
+#ifndef __LOGISIM_FILE_READER_H__
+#define __LOGISIM_FILE_READER_H__
+#include "CoreLib/XMLFile.h"
+#include "CoreLib/Files.h"
+#include "BaseLib/Int3.h"
+
+
+class CLogisimFileReader
+{
+protected:
+public:
+	void	Init(void);
+	void	Kill(void);
+};
+
+
+#endif // !__LOGISIM_FILE_READER_H__
+
